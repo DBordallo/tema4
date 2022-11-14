3cea0f8722bfc5dee437f715ae3913f73c9605cf
DBordallo <117821904+DBordallo@users.noreply.github.com>
2022-11-14 10:57:34 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("el número");
+        double numerolf = -1;
+
+        if (numerolf > 0) {
+            System.out.println("es positivo");
+        } else if (numerolf < 0) {
+            System.out.println("es negativo");
+        } else {
+            System.out.println("es neutro");        }
+    }
+
+}
